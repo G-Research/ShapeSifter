7b26d7f8d849608c08e696916c2a0fad1f0eb2f2
Patrick Stevens <3138005+Smaug123@users.noreply.github.com>
2024-05-30 22:42:38 +0100

    Release version 1.0 (#7)

--- .github/workflows/tag.sh
+++ .github/workflows/tag.sh
@@ -28,4 +28,65 @@ case "$TAG" in
 esac
 
 # the empty target_commitish indicates the repo default branch
-run_or_echo curl -L -X POST -H "Accept: application/vnd.github+json" -H "Authorization: Bearer $GITHUB_TOKEN" -H "X-GitHub-Api-Version: 2022-11-28" https://api.github.com/repos/G-Research/ShapeSifter/releases -d '{"tag_name":"'"$TAG"'","target_commitish":"","name":"'"$TAG"'","draft":false,"prerelease":false,"generate_release_notes":false}'
+curl_body='{"tag_name":"'"$TAG"'","target_commitish":"","name":"'"$TAG"'","draft":false,"prerelease":false,"generate_release_notes":false}'
+
+# process_curl_error expects no arguments, but reads the output of `curl` from the file `curl_output.json`.
+script_exit_code=-1
+process_curl_error() {
+    jq_query='if .errors | length == 1 then .errors[0].code else null end'
+    if ! jq -r --exit-status "$jq_query" curl_output.json; then
+        echo "Unexpectedly got number of errors not equal to 1"
+        cat curl_output.json
+        script_exit_code=3
+    else
+        github_error="$(jq -r "$jq_query" curl_output.json)"
+        echo "Error reported by GitHub: $github_error"
+
+        if [ "$github_error" != "already_exists" ]; then
+            echo "Unrecognised error message"
+            script_exit_code=4
+        else
+            # Expected: we tried to make a release that already exists, which can happen when path filters prevent a new tag
+            script_exit_code=0
+        fi
+    fi
+}
+
+test_process_curl_error() {
+    echo "Running tests."
+
+    failed_output=$(cat <<'EOF'
+{
+  "message": "Validation Failed",
+  "errors": [
+    {
+      "resource": "Release",
+      "code": "already_exists",
+      "field": "tag_name"
+    }
+  ],
+  "documentation_url": "https://docs.github.com/rest/releases/releases#create-a-release"
+}
+EOF
+)
+    echo "$failed_output" > curl_output.json
+    process_curl_error
+    if [ $script_exit_code != 0 ] ; then
+        echo "Test failure: got exit code $script_exit_code when we expected 0"
+        exit $script_exit_code
+    fi
+
+    echo "Tests completed."
+}
+
+if [[ "$DRY_RUN" = "1" ]]; then
+    test_process_curl_error
+else
+    if curl --fail-with-body -L -X POST -H "Accept: application/vnd.github+json" -H "Authorization: Bearer $GITHUB_TOKEN" -H "X-GitHub-Api-Version: 2022-11-28" https://api.github.com/repos/G-Research/ShapeSifter/releases -d "$curl_body" > curl_output.json ; then
+        cat curl_output.json
+        echo "cURL succeeded."
+    else
+        process_curl_error
+        exit $script_exit_code
+    fi
+fi
